152a63942cf52de5b2b4d6001bbc943aab3f9b84
anglesen1120 <tuan.nguyenminh2507@gmail.com>
2020-01-20 16:50:42 +0700

    update add file to project

--- src/app/components/masterdata/products/products.component.ts
+++ src/app/components/masterdata/products/products.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { UploadFile, NzMessageService } from 'ng-zorro-antd';
 import { HttpClient } from '@angular/common/http';
+import * as XLSX from "xlsx";
 
 interface ItemData {
   name: string;
@@ -17,15 +18,35 @@ export class ProductsComponent implements OnInit {
   uploading = false;
   fileList : UploadFile[] = [];
   listOfData: ItemData[] = [];
+  file : UploadFile;
+  
 
   constructor(private http: HttpClient, private messenger : NzMessageService) { }
 
-  beforUpload = (file: UploadFile): boolean => {
+  beforUpload = (file): boolean => {
     this.fileList = this.fileList.concat(file);
+    if(this.fileList.length >1)
+    {
+      this.messenger.info("Chỉ có thể chọn một file để import");
+      
+
+    }
+    
+    console.log("filename", file);
+    console.log("file-list",this.fileList);
     return false;
   }
 
   handleUpload (){
+    let workBook = null;
+    let jsonData = null;
+    const reader = new FileReader();
+
+
+  }
+
+  handleChange (file : UploadFile): void{
+    console.log("file name",file.name);
 
   }
 
